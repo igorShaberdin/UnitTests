86cdb76f08e7de88a67062dee1ebb40a406c60ce
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-21 16:46:12 +0300

    add task1

---
+++ java_tasks-1/unittest.java
@@ -0,0 +1,25 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class unittest {
+
+        @Test
+        public void shouldReturnCorrectValue() {
+            assertEquals(1, Solution.getMetreFromCentimetre(100), 0);
+        }
+
+        @Test
+        public void shouldReturnZeroValue() {
+            assertEquals(0, Solution.getMetreFromCentimetre(0), 0);
+        }
+
+        @Test
+        public void shouldReturnFloatValue() {
+            assertEquals(1.05, Solution.getMetreFromCentimetre(105), 0.000001);
+        }
+
+    }
+
