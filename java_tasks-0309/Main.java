8cb8984401c47996c685816bc5ee6ef547719239
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-23 13:16:59 +0300

    add task9

---
+++ java_tasks-0309/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int f = 0;
+        for(int i= 0; i<10; i++){
+            f = f + i + 1;
+            System.out.println(f);
+        }
+    }
+}
+
