ff14589f50578a8711afc840e89ea2a2079607c1
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-23 10:08:14 +0300

    add task05

---
+++ java_tasks-0305/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(iWasBornLongTimeAgo("MAY 17 1997"));
+    }
+    public static String iWasBornLongTimeAgo(String bornDate) {
+        return bornDate;
+    }
+}
