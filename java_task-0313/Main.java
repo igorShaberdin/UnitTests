92eee9b9712d0046dd2a32ad5614440773a06eef
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-23 17:57:11 +0300

    add task

---
+++ java_task-0313/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] words = new String[3][6];
+        words[1][1] = "мамаМылаРаму";
+        words[1][2] = "мылаМамаРаму";
+        words[1][3] = "мамаРамуМыла";
+        words[2][4] = "рамуМылаМама";
+        words[2][5] = "рамуМамаМыла";
+        words[2][6] = "мылаРамуМама";
+        System.out.println(words);
+    }
+}
+//доделаю завтра
