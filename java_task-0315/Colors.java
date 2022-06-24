3b93dbf211569de474bc9b2d82ce0bf9423bcb58
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-24 17:40:10 +0300

    add task15

---
+++ java_task-0315/Colors.java
@@ -0,0 +1,14 @@
+public class Colors {
+    String title;
+
+    public Colors(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Colors{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+}
