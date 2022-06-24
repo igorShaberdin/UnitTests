220734782eff04ec985724795a815767fa97e165
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-24 13:24:32 +0300

    remark

--- java_task-1/Main.java
+++ java_task-1/Main.java
@@ -1,5 +1,11 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getMetreFromCentimetre(243));
+    }
+
+    public static int getMetreFromCentimetre(int centimetre) {
+        return centimetre/100;
     }
 }
+
--- java_task-1/unittest.java
+++ java_task-1/unittest.java
@@ -1,25 +1,23 @@
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class unittest {
 
-        @Test
-        public void shouldReturnCorrectValue() {
-            assertEquals(1, Solution.getMetreFromCentimetre(100), 0);
-        }
-
-        @Test
-        public void shouldReturnZeroValue() {
-            assertEquals(0, Solution.getMetreFromCentimetre(0), 0);
-        }
-
-        @Test
-        public void shouldReturnFloatValue() {
-            assertEquals(1, Solution.getMetreFromCentimetre(105), 0.000001);
-        }
-
+    @Test
+    public void shouldReturnCorrectValue() {
+        assertEquals(1, Main.getMetreFromCentimetre(100), 0);
     }
 
+    @Test
+    public void shouldReturnZeroValue() {
+        assertEquals(0, Main.getMetreFromCentimetre(0), 0);
+    }
+
+    @Test
+    public void shouldReturnFloatValue() {
+        assertEquals(1, Main.getMetreFromCentimetre(105), 0.000001);
+    }
+
+}
