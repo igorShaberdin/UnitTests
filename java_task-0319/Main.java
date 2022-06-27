1de4375a8ac3b01628f4ef6b3eb1e148239ca26f
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-27 17:10:45 +0300

    add task19

--- java_task-0319/Main.java
+++ java_task-0319/Main.java
@@ -9,7 +9,9 @@ public class Main {
         int x = console.nextInt();
         System.out.println("Write the number2");
         int z = console.nextInt();
-        System.out.println(y+ "'s payment is " +x+ " in " +z+ " years");
+        printOut(y, x, z);
+    }
+    public static void printOut(String y, int x, int z) {
+        System.out.print(y+ "'s payment is $" +x+ " in " +z+ " years");
     }
 }
-
---
+++ java_task-0319/MainTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class MainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @BeforeEach
+    void setContent() {
+        System.setOut(new PrintStream(outContent));
+    }
+    @Test
+    void printOut() {
+        Main.printOut("Bob", 5000, 7);
+        assertEquals("Bob's payment is $5000 in 7 years", outContent.toString());
+    }
+}
