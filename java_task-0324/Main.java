3cd8d7ea7c8b505d9a3d0447d82c3b9854c41ef1
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-27 16:00:27 +0300

    remark24

--- java_task-0324/Main.java
+++ java_task-0324/Main.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "I want more many, that's why I'm learn Java";
         for(int i=0; i<10; i++) {
-            str = i;
-        }
+            String str = "I want more many, that's why I'm learn Java";
             System.out.println(str);
+        }
     }
 }
 
