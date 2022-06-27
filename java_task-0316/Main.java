fa4f64c30dca8f0a5aeb0069f643fd2788d71b36
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-27 15:15:04 +0300

    add task 16

---
+++ java_task-0316/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "\"C:\\Program Files\\Java\\jdk1.7.0\\bin\"";
+        String str1 = "\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\"";
+        System.out.println("It's Windows path: "+str);
+        System.out.println("It's Java string: "+str1);
+    }
+}
+
