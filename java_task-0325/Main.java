84ce43ab98e17ab857348e20abc8922e49111aed
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-27 16:08:05 +0300

    add task25

---
+++ java_task-0325/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("write the number");
+        int x = console.nextInt();
+        System.out.println("let's see... \n your payment will be $" +x+ " in hour");
+    }
+}
+
