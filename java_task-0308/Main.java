3213de0806889f778f7bba3eeba34ad738aa5472
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-28 10:28:22 +0300

    ake JUtests

--- java_task-0308/Main.java
+++ java_task-0308/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int f = 1;
+        int a = -1;
+        printOut(a);
+    }
+
+    public static void printOut(int f) {
+        f = 1;
         for(int i= 1; i<11; i++){
             f = f*i;
         }
--- java_task-0309/Main.java
+++ java_task-0309/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int f = 0;
+        int a = 1;
+        printOut(a);
+    }
+
+    public static void printOut(int f) {
+        f = 0;
         for(int i= 0; i<10; i++){
-            f = f + i + 1;
-            System.out.println(f);
+            f = f+i+1;
         }
+        System.out.print(f);
     }
 }
-
