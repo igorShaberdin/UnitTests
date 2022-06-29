8ffe242fbf6da453d66d425173ff23a3c2e1ae69
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-06-29 16:31:03 +0300

    add tasks

---
+++ task04/task0406/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    private static String fullName;
+
+    public static void setName(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+        Main.fullName = fullName;
+        System.out.print(fullName);
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
+
