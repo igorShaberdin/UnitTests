581de6db8aff6c18f230e7e2af8f072a67b52b6e
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-07-01 16:01:16 +0300

    add task

---
+++ task05/task0501/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String a = " ";
+        bio(a);
+    }
+    public static void bio (String a){
+        Cat cat1 = new Cat("Bob", 7, 6, 9);
+        Cat cat2 = new Cat("Nox", 3, 4, 6);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
