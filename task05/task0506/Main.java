1bec2cdd4c9d1d958c1d3fba80fe5490f8720c59
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-07-05 12:13:55 +0300

    add task

---
+++ task05/task0506/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        bioPerson();
+    }
+    public static void bioPerson (){
+        Person chel = new Person("Garold", 25, "mayami beach, 5, apr 47", 'm');
+        System.out.print(chel);
+    }
+}
---
+++ task05/task0506/MainTest.java
@@ -0,0 +1,20 @@
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import java.io.ByteArrayOutputStream;
+//import java.io.PrintStream;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//class MainTest {
+//    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//    @BeforeEach
+//    void setContent() {
+//        System.setOut(new PrintStream(outContent));
+//    }
+//    @Test
+//    void printOut() {
+//        Main.bio(" ");
+//        assertEquals("Bob 7 6 9\n" +
+//                "Nox 3 4 6\n" + "Dog 3 4 6\n", outContent.toString());
+//    }
+//}
+
